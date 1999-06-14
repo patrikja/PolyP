c2820afd311602ba50251e34df2991e508adbba8
mpc98 <mpc98>
1999-06-14 17:02:06 +0000

    Förklaring av hugs-flaggor + tillägg av -c70

--- src/hugs.mk
+++ src/hugs.mk
@@ -5,6 +5,11 @@
 
 hc = runhugs
 
+# The flags mean: 
+#   -c70  Use 70 iterations maximum for Class constraint simplification 
+#             (default 40 for Hugs98-May1999 was not enough)
+#   -98   Hugs mode (not Haskell 98 mode) needed to allow ST-monad extension
+#   -h300000 Heap size increased from default 200000 because of bug in Hugs-March1999
 default:
-	echo $(hc) -98 -h300000 `pwd`/Main.lhs $$\* >../bin/hugspolyp 
+	echo $(hc) -c70 -98 -h300000 `pwd`/Main.lhs $$\* >../bin/hugspolyp 
 	chmod +x ../bin/hugspolyp
